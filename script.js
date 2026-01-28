57adb1b1c9994a1f348c07b92644f0feff543583
Daringdeveloper25 <151807626+Daringdeveloper25@users.noreply.github.com>
2026-01-28 04:15:38 +0000

    Updated my portfolio to make it compose of both my web design and graphic design projects

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Reserved for future interactivity or enhancements
+
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+const closeBtn = document.querySelector('.close');
+const graphicDesignCards = document.querySelectorAll('.graphic-design-card');
+
+graphicDesignCards.forEach(card => {
+    card.addEventListener('click', () => {
+        const img = card.querySelector('.clickable-image');
+        modal.classList.add('show');
+        modalImage.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.remove('show');
+    }
+});
